f551b6c20bcf63774af30a61c3da716707a20aab
Jasmine Thompson <jasmine.mb.thompson@gmail.com>
2017-03-12 04:26:15 +0000

    Finish GUI tutorial

--- target.py
+++ target.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+
 import RPi.GPIO as GPIO
 import Tkinter
 import time
@@ -20,13 +23,42 @@ class simpleapp_tk(Tkinter.Tk):
     self.initialize()
 
   def initialize(self):
-    pass
+    self.grid()
+
+    self.entryVariable = Tkinter.StringVar()
+    self.entry = Tkinter.Entry(self, textvariable=self.entryVariable)
+    self.entry.grid(column=0, row=0, sticky='EW')
+    self.entry.bind("<Return>", self.OnPressEnter)
+    self.entryVariable.set(u"Enter text here.")
+
+    button = Tkinter.Button(self, text=u"Start Clock", command=self.OnButtonClick)
+    button.grid(column=1, row=0)
+
+    self.labelVariable = Tkinter.StringVar()
+    label = Tkinter.Label(self, textvariable=self.labelVariable, anchor="w", fg="white", bg="blue")
+    label.grid(column=0, row=1, columnspan=2, sticky='EW')
+    self.labelVariable.set(u"Hello !")
+
+    self.grid_columnconfigure(0, weight=1)
+    self.update()
+    self.geometry(self.geometry())
+    self.entry.focus_set()
+    self.entry.selection_range(0, Tkinter.END)
+
+  def OnButtonClick(self):
+    self.labelVariable.set(self.entryVariable.get() + " (You clicked the button !)")
+    self.entry.focus_set()
+    self.entry.selection_range(0, Tkinter.END)
+
+  def OnPressEnter(self, event):
+    self.labelVariable.set(self.entryVariable.get() + " (You pressed enter !)")
+    self.entry.focus_set()
+    self.entry.selection_range(0, Tkinter.END)
 
 if __name__ == "__main__":
-  print "start main"
   app = simpleapp_tk(None)
   app.title('my application')
-  print "end main"
+  app.mainloop()
 
 #if GPIO.input(TARGET_1):
 #  print 'input was HIGH'
