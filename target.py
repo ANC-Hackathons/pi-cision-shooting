067e4555720ed0a7e2727dfe9214b0929df3b33e
Jasmine Thompson <jasmine.mb.thompson@gmail.com>
2017-03-12 01:36:12 +0000

    Add Tkinter

--- target.py
+++ target.py
@@ -1,5 +1,5 @@
 import RPi.GPIO as GPIO
-#import Tkinter
+import Tkinter
 import time
 
 TARGET_1 = 6
@@ -13,71 +13,71 @@ GPIO.setup(TARGET_2, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TARGET_3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(TARGET_4, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-#class simpleapp_tk(Tkinter.Tk):
-#  def __init__(self, parent):
-#    Tkinter.Tk.__init__(self, parent)
-#    self.parent = parent
-#    self.initialize()
+class simpleapp_tk(Tkinter.Tk):
+  def __init__(self, parent):
+    Tkinter.Tk.__init__(self, parent)
+    self.parent = parent
+    self.initialize()
 
-#  def initialize(self):
-#    pass
+  def initialize(self):
+    pass
 
-#if __name__ == "__main__":
-#  print "start main"
-#  app = simpleapp_tk(None)
-#  app.title('my application')
-#  print "end main"
+if __name__ == "__main__":
+  print "start main"
+  app = simpleapp_tk(None)
+  app.title('my application')
+  print "end main"
 
-if GPIO.input(TARGET_1):
-  print 'input was HIGH'
-else:
-  print 'input was LOW'
+#if GPIO.input(TARGET_1):
+#  print 'input was HIGH'
+#else:
+#  print 'input was LOW'
 
-if GPIO.input(TARGET_2):
-  print 'input was HIGH'
-else:
-  print 'input was LOW'
+#if GPIO.input(TARGET_2):
+#  print 'input was HIGH'
+#else:
+#  print 'input was LOW'
 
-if GPIO.input(TARGET_3):
-  print 'input was HIGH'
-else:
-  print 'input was LOW'
+#if GPIO.input(TARGET_3):
+#  print 'input was HIGH'
+#else:
+#  print 'input was LOW'
 
-if GPIO.input(TARGET_4):
-  print 'input was HIGH'
-else:
-  print 'input was LOW'
+#if GPIO.input(TARGET_4):
+#  print 'input was HIGH'
+#else:
+#  print 'input was LOW'
 
-start_time = time.time()
-print 'Started timer'
-while GPIO.input(TARGET_1) == GPIO.HIGH or \
-      GPIO.input(TARGET_2) == GPIO.HIGH or \
-      GPIO.input(TARGET_3) == GPIO.HIGH or \
-      GPIO.input(TARGET_4) == GPIO.HIGH:
+#start_time = time.time()
+#print 'Started timer'
+#while GPIO.input(TARGET_1) == GPIO.HIGH or \
+#      GPIO.input(TARGET_2) == GPIO.HIGH or \
+#      GPIO.input(TARGET_3) == GPIO.HIGH or \
+#      GPIO.input(TARGET_4) == GPIO.HIGH:
 
-  time.sleep(0.2)
+#  time.sleep(0.2)
 
-  if GPIO.input(TARGET_1):
-    print 'target 1 input was HIGH'
-  else:
-    print 'target 1 input was LOW'
+#  if GPIO.input(TARGET_1):
+#    print 'target 1 input was HIGH'
+#  else:
+#    print 'target 1 input was LOW'
 
-  if GPIO.input(TARGET_2):
-    print 'target 2 input was HIGH'
-  else:
-    print 'target 2 input was LOW'
+#  if GPIO.input(TARGET_2):
+#    print 'target 2 input was HIGH'
+#  else:
+#    print 'target 2 input was LOW'
 
-  if GPIO.input(TARGET_3):
-    print 'target 3 input was HIGH'
-  else:
-    print 'target 3 input was LOW'
+#  if GPIO.input(TARGET_3):
+#    print 'target 3 input was HIGH'
+#  else:
+#    print 'target 3 input was LOW'
 
-  if GPIO.input(TARGET_4):
-    print 'target 4 input was HIGH'
-  else:
-    print 'target 4 input was LOW'
+#  if GPIO.input(TARGET_4):
+#    print 'target 4 input was HIGH'
+#  else:
+#    print 'target 4 input was LOW'
 
-end_time = time.time()
-print 'Stopped time'
-print 'Time: ' + str(end_time - start_time)
-GPIO.cleanup()
+#end_time = time.time()
+#print 'Stopped time'
+#print 'Time: ' + str(end_time - start_time)
+#GPIO.cleanup()
